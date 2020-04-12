6a74690b076fe41dfaa4519887279625d74bd70b
syf767g <63532264+syf767g@users.noreply.github.com>
2020-04-12 09:06:34 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,150 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "#";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`صقور العرب`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "مسح")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم مسح الشات",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل ",
+        footer: {
+          text: "©zabhm"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+});
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'السلام عليكم') {
+              message.channel.send('**وعليكم السلام ولرحمة الله وبركاته منور**');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'باك') {
+              message.channel.send('**ولكم منور**');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'هاي') {
+              message.channel.send('**هاي يا عسل**');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'فيه قيم') {
+              message.channel.send('**اذا في قيم بنقول**');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'ادمن') {
+              message.channel.send('**الادمن-
+@syf6776
+@samer_h7SZB
+@ bablo_F-16
+@ faisalsehele
+@sary1200
+@ II-iTz-AyooD-II
+@ FJ3NE**');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === '..') {
+              message.channel.send('**مسؤول التوظيف 
+
+مسؤول توظيف العساكر عند 
+الحاكم
+_________________________________
+مسؤول توظيف العصابات عند 
+@samer_h7SZB
+_______________________________
+@everyone**');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'المجتمع') {
+              message.channel.send('**<#682569625392644105>**');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+
+client.login(process.env.BOT_TOKEN);
